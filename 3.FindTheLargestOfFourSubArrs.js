fcd7b8dc865f49588a509dd738e8fc47314aed54
Lewis Betances <patience174@hotmail.com>
2018-09-28 20:49:06 -0700

    ftf

--- 3.FindTheLargestOfFourSubArrs.js
+++ 3.FindTheLargestOfFourSubArrs.js
@@ -90,3 +90,20 @@ largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 85
     }, 0); // 0 serves as the context for the first pLN in each sub array
   });
 }*/
+
+
+
+//Approach #3: Return the Largest Numbers in a Array With Built-In Functions — with map() and apply()
+
+function largestOfFour(mainArray) {
+  // Step 1. Map over the main arrays
+  return mainArray.map(function(subArray) { // Step 3. Return the largest numbers of each sub-arrays => returns [5,27,39,1001]
+    
+    // Step 2. Return the largest numbers for each sub-arrays with Math.max() method
+    return Math.max.apply(null, subArray);
+  });
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
