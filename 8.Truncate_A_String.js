2f0f36c1e94503d6b5946bbd65526a7f89acc26d
Lewis Betances <patience174@hotmail.com>
2018-10-08 03:06:21 -0700

    8

--- 8.Truncate_A_String.js
+++ 8.Truncate_A_String.js
@@ -1,22 +1,9 @@
 function truncateString(str, num) {
-  // Clear out that junk in your trunk
-  if (str.length > num && num > 3) {
-    return str.slice(0, (num - 3)) + '...';
-  } else if (str.length > num && num <= 3) {
-    return str.slice(0, num) + '...';
-  } else {
-    return str;
-  }
-}
-console.log(
-truncateString("This sentence is insane on its own...",15));
 
-function truncateString(str, num) {
-  if (str.length <= num) {
-    return str;
-  } else {
-    return str.slice(0, num > 3 ? num - 3 : num) + '...';
-  }
+  if(num >= str.length) return str;
+  return str.slice(0,num)+"...";
 }
 console.log(
-truncateString("This sentence is insane on its own...",15));
+truncateString("Green and yellow basket", 18));
+
+
